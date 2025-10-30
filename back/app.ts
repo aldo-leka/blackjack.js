eb8ebbc46c71073b232aecaaf569d0ad7c6ec8d7
Aldo Leka <aldo.leka@live.com>
2025-10-30 16:11:27 +0100

    Nickname handshaking

--- back/app.ts
+++ back/app.ts
@@ -5,15 +5,18 @@ import { errorHandler } from './middlewares/errorHandler';
 import cors from "cors";
 import config from './config/config';
 import { createServer } from 'node:http';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./auth";
+import { logInfo, logWarning } from './log';
+import { IpApiResponse } from './models/ip-api';
 
 const app = express();
 
 app.use(
     cors({
         origin: config.frontendUrl,
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true, // Allow credentials (cookies, authorization headers, etc.)
     })
 );
@@ -30,25 +33,94 @@ app.use('/api/users', userRoutes);
 app.use(errorHandler);
 
 const server = createServer(app);
-const io = new Server(server);
-
-io.on('connection', async (socket) => {
-    socket.on('chat message', async (msg) => {
-        let result;
-        try {
-            // store the message in the database
-            // result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
-        } catch (e) {
-            // TODO handle the failure
-            return;
-        }
-        // include the offset with the message
-        io.emit('chat message', msg);
-    });
-
-    if (!socket.recovered) {
-        // if the connection state recovery was not successful
+const io = new Server(server, {
+    cors: {
+        origin: config.frontendUrl,
+        methods: ["GET", "POST"],
+        credentials: true,
     }
 });
+
+/** key: nickname, value: { socketId, countryCode, room, etc. } */
+let users = new Map();
+users.set("aldo", {}); // todo remove after testing...
+
+/** key: nickname, value: timer id */
+let disconnects = new Map();
+
+io.on('connection', async (socket) => {
+    socket.on("register nickname", async (nickname) => {
+        const existing = users.get(nickname)
+
+        if (existing && existing.socketId !== socket.id) {
+            if (!disconnects.has(nickname)) {
+                socket.emit('nickname unavailable');
+                return;
+            }
+
+            clearTimeout(disconnects.get(nickname));
+            disconnects.delete(nickname);
+
+            logInfo(`${nickname} from ${existing.countryCode} reconnected before timeout`);
+        }
+
+        socket.data.nickname = nickname;
+
+        const ip = getIp(socket);
+        const countryCode = existing?.countryCode || await getCountryCodeFromIP(ip);
+
+        users.set(nickname, {
+            ...(existing || {}),
+            socketId: socket.id,
+            countryCode: countryCode
+        });
+
+        logInfo(`on nickname handshake: ${nickname} from ${countryCode} (ip: ${ip})`);
+
+        socket.emit('nickname accepted');
+    });
+
+    socket.on('disconnect', () => {
+        const nickname = socket.data.nickname;
+        if (!nickname) {
+            return;
+        }
+
+        const timeoutId = setTimeout(() => {
+            const countryCode = users.get(nickname).countryCode;
+            users.delete(nickname);
+            disconnects.delete(nickname);
+            logInfo(`${nickname} from ${countryCode} (ip: ${socket.handshake.address}) removed after timeout`);
+        }, 30_000);
+
+        disconnects.set(nickname, timeoutId);
+
+        logInfo(`${nickname} marked as a disconnect`);
+    })
+});
+
+function getIp(socket: Socket) {
+    const forwardedForHeaderValue = socket.handshake.headers['x-forwarded-for'];
+    if (forwardedForHeaderValue && typeof forwardedForHeaderValue === 'string') {
+        return forwardedForHeaderValue.split(',')[0].trim()
+            || socket.handshake.address;
+    }
+    else if (forwardedForHeaderValue && Array.isArray(forwardedForHeaderValue)) {
+        return forwardedForHeaderValue[0].trim() || socket.handshake.address;
+    }
+
+    return socket.handshake.address;
+}
+
+async function getCountryCodeFromIP(ip: string) {
+    try {
+        const res = await fetch(`http://ip-api.com/json/${ip}`);
+        const data = await res.json() as IpApiResponse;
+        if (data.status === 'success') return data.countryCode;
+    } catch (err) {
+        logWarning("Geo lookup failed", err);
+    }
+    return null;
+}
 
 export default server;
---
+++ front/src/app/set-nickname/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { socket } from "@/lib/socket";
+import { useRouter, useSearchParams } from "next/navigation";
+
+export default function Page() {
+    const [nickname, setNickname] = useState("");
+    const [error, setError] = useState("");
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        function handleNicknameUnavailable() {
+            setError("Nickname is taken");
+        }
+
+        function handleNicknameAccepted() {
+            console.log(`at set-username page. setting nickname ls to ${nickname}`)
+            localStorage.setItem("nickname", nickname);
+            const returnUrl = searchParams.get('returnUrl') || '/';
+            router.push(returnUrl);
+        }
+
+        socket.on("nickname unavailable", handleNicknameUnavailable);
+        socket.on("nickname accepted", handleNicknameAccepted);
+
+        const lsNickname = localStorage.getItem('nickname');
+        if (lsNickname && lsNickname.trim() !== "") {
+            setNickname(lsNickname);
+            socket.emit("register nickname", lsNickname);
+        }
+
+        return () => {
+            socket.off("nickname unavailable", handleNicknameUnavailable);
+            socket.off("nickname accepted", handleNicknameAccepted);
+        }
+    }, [router, searchParams, nickname]);
+
+    return (
+        <div className="flex flex-col items-center gap-4 bg-[url(/images/table.png)] bg-cover bg-center min-h-screen">
+            <h2 className="text-[#DAA520] text-3xl font-bold m-4">Blackjack</h2>
+            <div className="flex">
+                <input
+                    className="bg-[#00000044] text-white placeholder-white px-3 py-1"
+                    placeholder="Enter your nickname"
+                    value={nickname}
+                    onChange={(e) => setNickname(e.target.value)}
+                />
+                <button
+                    onClick={() => socket.emit("register nickname", nickname)}
+                    disabled={!nickname.trim()}
+                    className="w-8 text-[#016F32] font-semibold bg-[#DAA520] disabled:opacity-50"
+                >
+                    {'>'}
+                </button>
+            </div>
+            {error && (
+                <p className="text-sm text-red-500 mt-2">{error}</p>
+            )}
+        </div>
+    );
+}
