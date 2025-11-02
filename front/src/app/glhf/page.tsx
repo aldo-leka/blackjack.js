00bcef2bdabfe2341aeff1134aa9bcffdd3ea72d
Aldo Leka <aldo.leka@live.com>
2025-11-02 12:35:30 +0100

    fixed betting

--- front/src/app/glhf/page.tsx
+++ front/src/app/glhf/page.tsx
@@ -5,7 +5,7 @@ import { socket } from "@/lib/socket";
 import { Repeat } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useNickname } from "@/contexts/NicknameContext";
-import { CHIPS } from "@/lib/constants";
+import { CHIPS } from "@/lib/util";
 
 interface Player {
     nickname: string;
@@ -137,10 +137,6 @@ export default function Page() {
     }, [isHandshakeComplete]);
 
     const cash = (worth ?? 0) - (bet ?? 0);
-    let chips = [0, 0, 0, 0, 0];
-    if (cash) {
-        chips = convertToChips(cash);
-    }
 
     let betChips = [0, 0, 0, 0, 0];
     if (bet) {
@@ -166,14 +162,14 @@ export default function Page() {
     }
 
     function addBet(index: number) {
-        if (chips[index] > 0) {
+        if (CHIPS[index] <= cash) {
             setBet(prev => (prev ?? 0) + CHIPS[index]);
             socket.emit("change bet", index, "add");
         }
     }
 
     function removeBet(index: number) {
-        if (betChips[index] > 0) {
+        if (bet && CHIPS[index] <= bet) {
             setBet(prev => prev! - CHIPS[index]);
             socket.emit("change bet", index, "remove");
         }
@@ -205,71 +201,71 @@ export default function Page() {
             <div className="grid grid-cols-2 gap-4">
                 <div className="flex flex-col items-center gap-2">
                     <h2 className="text-white italic font-semibold">
-                        You {worth && <>({worth})</>}
+                        You {worth && <>(${worth})</>}
                     </h2>
                     <div className="flex gap-1.5">
                         <div className="flex flex-col gap-2">
-                            <Chip color="white" amount={chips[0]} />
+                            <Chip color="white" amount={CHIPS[0]} />
                             <button
                                 onClick={() => addBet(0)}
-                                className={`${chips[0] === 0 ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
+                                className={`${CHIPS[0] > cash ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
                                 +
                             </button>
                             <button
                                 onClick={() => removeBet(0)}
-                                className={`${betChips[0] === 0 ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
+                                className={`${!bet || (bet < CHIPS[0]) ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
                                 -
                             </button>
                         </div>
                         <div className="flex flex-col gap-2">
-                            <Chip color="red" amount={chips[1]} />
+                            <Chip color="red" amount={CHIPS[1]} />
                             <button
                                 onClick={() => addBet(1)}
-                                className={`${chips[1] === 0 ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
+                                className={`${CHIPS[1] > cash ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
                                 +
                             </button>
                             <button
                                 onClick={() => removeBet(1)}
-                                className={`${betChips[1] === 0 ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
+                                className={`${!bet || (bet < CHIPS[1]) ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
                                 -
                             </button>
                         </div>
                         <div className="flex flex-col gap-2">
-                            <Chip color="green" amount={chips[2]} />
+                            <Chip color="green" amount={CHIPS[2]} />
                             <button
                                 onClick={() => addBet(2)}
-                                className={`${chips[2] === 0 ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
+                                className={`${CHIPS[2] > cash ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
                                 +
                             </button>
                             <button
                                 onClick={() => removeBet(2)}
-                                className={`${betChips[2] === 0 ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
+                                className={`${!bet || (bet < CHIPS[2]) ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
                                 -
                             </button>
                         </div>
                         <div className="flex flex-col gap-2">
-                            <Chip color="black" amount={chips[3]} />
+                            <Chip color="black" amount={CHIPS[3]} />
                             <button
                                 onClick={() => addBet(3)}
-                                className={`${chips[3] === 0 ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
+                                className={`${CHIPS[3] > cash ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
                                 +
                             </button>
                             <button
                                 onClick={() => removeBet(3)}
-                                className={`${betChips[3] === 0 ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
+                                className={`${!bet || (bet < CHIPS[3]) ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
                                 -
                             </button>
                         </div>
                         <div className="flex flex-col gap-2">
-                            <Chip color="blue" amount={chips[4]} />
+                            <Chip color="blue" amount={CHIPS[4]} />
                             <button
                                 onClick={() => addBet(4)}
-                                className={`${chips[4] === 0 ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
+                                className={`${CHIPS[4] > cash ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
                                 +
                             </button>
                             <button
                                 onClick={() => removeBet(4)}
-                                className={`${betChips[4] === 0 ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
+                                className={`${!bet || (bet < CHIPS[4]) ? "opacity-50" : ""} bg-[#DAA520] rounded-sm cursor-pointer text-[#016F32]`}>
                                 -
                             </button>
                         </div>
@@ -291,9 +287,12 @@ export default function Page() {
                                 className="transition-all duration-1000 ease-linear"
                             />
                         </svg>
-                        
-                        <div className="bg-[#daa52080] rounded-full size-36 border-4 border-transparent">
-                            <div className="flex justify-center items-center h-full">
+
+                        <div className="grid grid-rows-3 bg-[#daa52080] rounded-full size-36 border-4 border-transparent">
+                            <div className="flex justify-center items-end text-white italic font-semibold pb-1">
+                                {bet ? `$${bet}` : ''}
+                            </div>
+                            <div className="flex justify-center items-center">
                                 <Chip color="white" amount={betChips[0]} />
                                 <Chip color="red" amount={betChips[1]} />
                                 <Chip color="green" amount={betChips[2]} />
@@ -317,7 +316,7 @@ export default function Page() {
                 {otherPlayers.length > 0 &&
                     <div className={`${otherPlayers[0].disconnected ? "opacity-50" : ""} flex flex-col items-center gap-2`}>
                         <h2 className="text-white italic font-semibold">
-                            {otherPlayers[0].nickname} ({otherPlayers[0].worth})
+                            {otherPlayers[0].nickname} (${otherPlayers[0].worth})
                         </h2>
                         <div className="bg-[#daa52039] rounded-full size-36">
                             <div className="flex justify-center items-center h-full">
@@ -343,7 +342,7 @@ export default function Page() {
                 {otherPlayers.length > 1 &&
                     <div className={`${otherPlayers[1].disconnected ? "opacity-50" : ""} flex flex-col items-center gap-2`}>
                         <h2 className="text-white italic font-semibold">
-                            {otherPlayers[1].nickname} ({otherPlayers[1].worth})
+                            {otherPlayers[1].nickname} (${otherPlayers[1].worth})
                         </h2>
                         <div className="bg-[#daa52039] rounded-full size-36">
                             <div className="flex justify-center items-center h-full">
